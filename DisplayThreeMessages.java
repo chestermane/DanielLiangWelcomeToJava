40e511ff474c040bcff3c38c169b6e23ba3dbf11
chestermane <chestermane@gmail.com>
2017-05-11 23:53:17 -0400

    Add files via upload

---
+++ DisplayThreeMessages.java
@@ -0,0 +1,20 @@
+package myprograminglab;
+
+/*
+ * Write a program that displays Welcome to Java,
+ * Welcome to Computer Science, and Programming is fun.
+ */
+
+/**
+ *
+ * @author Evan
+ */
+public class DisplayThreeMessages {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Computer Science.");
+        System.out.println("Programming is fun.");
+    }
+
+}
