e3a74afe262e7f3fe810e19e2270272f4a92c963
chestermane <chestermane@gmail.com>
2017-05-12 00:45:44 -0400

    second

---
+++ DisplayFiveMessages.java
@@ -0,0 +1,18 @@
+/*
+ * Write a program that displays Welcome to Java five times.
+ */
+
+/**
+ *
+ * @author Evan
+ */
+public class DisplayFiveMessages {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java.");
+        }
+    }
+
+}
