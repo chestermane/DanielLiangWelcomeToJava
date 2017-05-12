4bfad692176459f034d474bfb6377da6bda38c4a
chestermane <chestermane@gmail.com>
2017-05-12 01:12:16 -0400

    three

---
+++ DisplayPattern.java
@@ -0,0 +1,18 @@
+/*
+ * Write a program that displays Welcome to Java five times.
+ */
+
+/**
+ *
+ * @author Evan
+ */
+public class DisplayPattern {
+
+    public static void main(String[] args) {
+
+        System.out.println("    J\t A   V     V   A\n    J   A A   V   V   "
+                + "A A\nJ   J  AAAAA   V V   AAAAA" + "\n J J  A     A   V   A"
+                + "     A");
+
+    }
+}
