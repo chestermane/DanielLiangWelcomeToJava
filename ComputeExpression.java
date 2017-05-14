a5b56d59be21d76e85aa87575e8a03037d86bc16
chestermane <chestermane@gmail.com>
2017-05-14 00:34:28 -0400

    1.5

---
+++ ComputeExpression.java
@@ -0,0 +1,18 @@
+/*
+ * 1.5 (Compute expressions) Write a program that displays the result of 9.5 * 
+ * 4.5 - 2.5 * 3 / 45.5 - 3.5.
+ */
+package computeexpression;
+
+/**
+ *
+ * @author Evan
+ */
+public class ComputeExpression {
+
+    public static void main(String[] args) {
+        double result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+        System.out.println(result);
+    }
+
+}
