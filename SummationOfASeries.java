11a5293b5c11f7adc0f45efb021e370b643f6ca2
chestermane <chestermane@gmail.com>
2017-05-14 00:47:25 -0400

    1.6

---
+++ SummationOfASeries.java
@@ -0,0 +1,18 @@
+/*
+ * 1.6 (Summation of a series) Write a program that displays the result of
+ * 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9..
+ */
+package summationofaseries;
+
+/**
+ *
+ * @author Evan
+ */
+public class SummationOfASeries {
+
+    public static void main(String[] args) {
+        System.out.println("Your total is: " + (1 + 2 + 3 + 4 + 5 + 6 + 7
+                + 8 + 9));
+    }
+
+}
